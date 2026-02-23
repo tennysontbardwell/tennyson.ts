f1050e8530949f1326495d12419cfe87dc464ccf
Tennyson T Bardwell <tennysontaylorbardwell@gmail.com>
2026-02-22 22:54:23 -0600

    reorg hq route handling

--- tennyson/app/hq/App.tsx
+++ tennyson/app/hq/App.tsx
@@ -1,12 +1,70 @@
-import React from "react";
+import { ViewerApp } from "tennyson/app/hq/viewer";
+import { HQQuickDev } from "tennyson/app/hq/hq-quickdev";
+import {
+  Outlet,
+  createRoute,
+  createRootRoute,
+  Link,
+} from "@tanstack/react-router";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello from React!</h1>
-      <p>This component is rendered by Vite.</p>
-    </div>
-  );
-}
+export const tabs = [
+  {
+    name: "Home",
+    path: "/",
+    component: () => <h3>Welcome Home!</h3>,
+  },
+  {
+    name: "About",
+    path: "/about",
+    component: () => <h3>This is the About page.</h3>,
+  },
+  {
+    name: "ImageViewer",
+    path: "/imgviewer",
+    component: ViewerApp,
+  },
+  {
+    name: "HQ QuickDev",
+    path: "/hq-quickdev",
+    component: HQQuickDev,
+  },
+];
 
-export default App;
+export const routeTreeOfTabs = (ts: typeof tabs) => {
+  const rootRoute = createRootRoute({
+    component: () => (
+      <div
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <div
+          style={{ flex: 0, display: "flex", gap: "0.5rem", padding: "0.5rem" }}
+        >
+          {ts.map((tab) => (
+            <Link
+              key={tab.path}
+              to={tab.path}
+              activeProps={{ style: { fontWeight: "bold" } }}
+            >
+              {tab.name}
+            </Link>
+          ))}
+        </div>
+        <div style={{ flex: 1, minHeight: 0, padding: "0.5rem" }}>
+          <Outlet />
+        </div>
+      </div>
+    ),
+  });
+
+  const routeTree = rootRoute.addChildren([
+    ...ts.map((tab) =>
+      createRoute({
+        getParentRoute: () => rootRoute,
+        path: tab.path,
+        component: tab.component,
+      }),
+    ),
+  ]);
+
+  return routeTree;
+};
--- tennyson/app/hq/routers.tsx
+++ tennyson/app/hq/routers.tsx
@@ -1,74 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  Outlet,
   RouterProvider,
   createRouter,
-  createRoute,
-  createRootRoute,
-  Link,
 } from "@tanstack/react-router";
 
-import { ViewerApp } from "tennyson/app/hq/viewer";
+import { tabs, routeTreeOfTabs } from "tennyson/app/hq/App";
 
-const rootRoute = createRootRoute({
-  component: () => (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <div
-        style={{ flex: 0, display: "flex", gap: "0.5rem", padding: "0.5rem" }}
-      >
-        <Link to="/" activeProps={{ style: { fontWeight: "bold" } }}>
-          Home
-        </Link>
-        <Link to="/about" activeProps={{ style: { fontWeight: "bold" } }}>
-          About
-        </Link>
-        <Link to="/imgviewer" activeProps={{ style: { fontWeight: "bold" } }}>
-          ImageViewer
-        </Link>
-      </div>
-      <div style={{ flex: 1, minHeight: 0, padding: "0.5rem" }}>
-        <Outlet />
-      </div>
-    </div>
-  ),
-});
+import "tennyson/app/hq/main.css"
 
-const routeTree = rootRoute.addChildren([
-  createRoute({
-    getParentRoute: () => rootRoute,
-    path: "/",
-    component: function Index() {
-      return <h3>Welcome Home!</h3>;
-    },
-  }),
-  createRoute({
-    getParentRoute: () => rootRoute,
-    path: "/about",
-    component: function About() {
-      return <h3>This is the About page.</h3>;
-    },
-  }),
-  createRoute({
-    getParentRoute: () => rootRoute,
-    path: "/imgviewer",
-    component: ViewerApp,
-  }),
-]);
+const router = createRouter({ routeTree: routeTreeOfTabs(tabs) });
 
-// 4. Create the Router
-// This router instance will manage your application's state.
-const router = createRouter({ routeTree });
-
-// Register the router for maximum type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-// 5. Render the App
-// Use the `<RouterProvider>` component to provide the router to your app.
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
